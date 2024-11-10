53a59842fac1e90a31430b028c30ee01fb1cee0e
alusamuel <s.rurangami@alustudent.com>
2024-11-10 15:51:33 -0800

    python input and output 1-12

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-""" Module that make student class """
-
-
+"""Module that defines a Student class."""
 
 class Student:
+    """Represents a student with first name, last name, and age."""
+
     def __init__(self, first_name, last_name, age):
-       """Initializes a student instance."""
+        """Initializes a student instance with given attributes."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Returns the student's attributes as a dictionary."""
-        if attrs is None:
-            return self.__dict__
-        else:
-            return {key: value for key, value in self.__dict__.items() if key in attrs}
+        """Returns selected attributes as a dictionary if `attrs` is given."""
+        return {k: self.__dict__[k] for k in (attrs or self.__dict__)}
