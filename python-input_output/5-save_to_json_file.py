2037a812c350c449092128868957e66465a0f350
alusamuel <s.rurangami@alustudent.com>
2024-11-10 15:08:43 -0800

    python input and output 1-12

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
+"""
+Module to save a Python object as a JSON file.
+"""
+
 import json
 
 
 def save_to_json_file(my_obj, filename):
+    """
+    Writes a Python object to a file as a JSON string.
+
+    Args:
+        my_obj: The Python object to be written to the file.
+        filename (str): The name of the file where the object will be saved.
+    """
     with open(filename, "w", encoding="utf-8") as file:
         json.dump(my_obj, file)
