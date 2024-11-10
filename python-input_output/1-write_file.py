517b2893dbb5981665c6b89afebd7a2b55cba62e
alusamuel <s.rurangami@alustudent.com>
2024-11-10 14:55:14 -0800

    python input and output 1-12

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
-"""This is write file function"""
+"""This module contains a function to write text to a file."""
+
 
 def write_file(filename="", text=""):
+    """
+    Writes text to a file.
+
+    Args:
+        filename (str): The file to write to (default is an empty string).
+        text (str): The text to write.
+    
+    Returns:
+        int: The number of characters written.
+    """
     with open(filename, "w", encoding="utf-8") as file:
         return file.write(text)
