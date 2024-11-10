0ff1899ad70fc2c3d1782359bb5af392e838d4f6
alusamuel <s.rurangami@alustudent.com>
2024-11-10 15:24:23 -0800

    python input and output 1-12

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
+"""
+Student class with a method to return selected attributes as a dictionary.
+"""
 
 
 class Student:
+    """
+    Represents a student with first name, last name, and age.
+    """
     def __init__(self, first_name, last_name, age):
+       """Initializes a student instance."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
+        """
+        Returns the student's attributes as a dictionary.
+        If `attrs` is given, only those attributes are returned.
+        """
         if attrs is None:
             return self.__dict__
         else:
