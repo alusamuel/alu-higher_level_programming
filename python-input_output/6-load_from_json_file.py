0810ab66a9acc1bd5c32613756ac930df25d50f0
alusamuel <s.rurangami@alustudent.com>
2024-11-10 15:10:47 -0800

    python input and output 1-12

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+"""
+Module to load a Python object from a JSON file.
+"""
+
 import json
 
 
 def load_from_json_file(filename):
+    """
+    Reads a Python object from a JSON file.
+
+    Args:
+        filename (str): The name of the file to read the JSON string from.
+    """
     with open(filename, "r", encoding="utf-8") as file:
         return json.load(file)
