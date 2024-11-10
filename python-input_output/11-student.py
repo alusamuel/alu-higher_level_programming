3eedf99f4b6387971eaa89a817d5d968156642fc
alusamuel <s.rurangami@alustudent.com>
2024-11-10 14:45:37 -0800

    python input and output 1-12

---
+++ python-input_output/11-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance."""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {key: value for key, value in self.__dict__.items() if key in attrs}
+
+    def reload_from_json(self, json):
+        """Reloads attributes from a dictionary."""
+        for key, value in json.items():
+            setattr(self, key, value)
