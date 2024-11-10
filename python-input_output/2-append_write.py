fab2ed3c50b573e83172495a13e1d77ac93882d0
alusamuel <s.rurangami@alustudent.com>
2024-11-10 15:00:15 -0800

    python input and output 1-12

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
-"""This is append file function"""
+"""This module contains a function to append text to a file."""
 
 
 def append_write(filename="", text=""):
+    """
+    Appends text to a file.
+
+    Args:
+        filename (str): The file to append to (default is an empty string).
+        text (str): The text to append.
+    
+    Returns:
+        int: The number of characters added.
+    """
     with open(filename, "a", encoding="utf-8") as file:
         return file.write(text)
