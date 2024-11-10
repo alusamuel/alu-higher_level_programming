1295a0a2010226454dd6ceb70c48572c52c88329
alusamuel <s.rurangami@alustudent.com>
2024-11-10 14:53:16 -0800

    python input and output 1-12

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-"""This is readfile"""
+"""This module contains a function to read and print a file."""
 
 
 def read_file(filename=""):
+    """
+    Reads and prints the content of a file.
+
+    Args:
+        filename (str): The file to read (default is an empty string).
+    """
     with open(filename, "r", encoding="utf-8") as file:
         print(file.read(), end="")
