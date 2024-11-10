679878e9b4bfc6a1b7f6346de29402a1629f178e
alusamuel <s.rurangami@alustudent.com>
2024-11-10 14:57:10 -0800

    python input and output 1-12

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -9,9 +9,6 @@ def write_file(filename="", text=""):
     Args:
         filename (str): The file to write to (default is an empty string).
         text (str): The text to write.
-    
-    Returns:
-        int: The number of characters written.
     """
     with open(filename, "w", encoding="utf-8") as file:
         return file.write(text)
