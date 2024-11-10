8774067db135ca8cc60b43a3380c670e573deecd
alusamuel <s.rurangami@alustudent.com>
2024-11-10 15:01:07 -0800

    python input and output 1-12

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -9,9 +9,6 @@ def append_write(filename="", text=""):
     Args:
         filename (str): The file to append to (default is an empty string).
         text (str): The text to append.
-    
-    Returns:
-        int: The number of characters added.
     """
     with open(filename, "a", encoding="utf-8") as file:
         return file.write(text)
