f209a3cb038549df0e3d5a844891a56bbecaffcd
alusamuel <s.rurangami@alustudent.com>
2024-11-10 15:07:17 -0800

    python input and output 1-12

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""
+Module to convert a JSON string back into a Python object.
+"""
+
 import json
 
 
 def from_json_string(my_str):
+    """
+    Converts a JSON string to a Python object.
+
+    Args:
+        my_str (str): The JSON string to be converted.
+    """
     return json.loads(my_str)
