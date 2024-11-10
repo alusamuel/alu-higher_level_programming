6b828babf9d59fd9cd86e24af68a4209d041f36d
alusamuel <s.rurangami@alustudent.com>
2024-11-10 15:19:29 -0800

    python input and output 1-12

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""
+Module to retrieve the dictionary representation of an object.
+"""
 
 
 def class_to_json(obj):
+    """
+    Returns the dictionary representation of an object.
+
+    Args:
+        obj: The object to be converted.
+    """
     return obj.__dict__
