60cc19d36da2f6b152946ae5b2fe347ee9d22574
alusamuel <s.rurangami@alustudent.com>
2024-11-16 07:19:29 -0800

    SQL - Introduction

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,8 +1,8 @@
--- Convert the database to utf8mb4
+-- Convert the database to UTF8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
--- Convert the table to utf8mb4
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Convert the table to UTF8
+ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert the 'name' column to utf8mb4
-ALTER TABLE first_table MODIFY COLUMN name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
+-- Modify the `name` column to UTF8
+ALTER TABLE hbtn_0c_0.first_table MODIFY COLUMN `name` VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
